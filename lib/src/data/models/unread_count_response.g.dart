2b15424beae2812d32479fa3cce46a3e51ea7077
Gbogboade Ayomide <ay4allcrown@gmail.com>
2025-11-11 10:03:45 +0100

    fix: add missing freezed files

---
+++ lib/src/data/models/unread_count_response.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'unread_count_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_UnreadCountResponse _$UnreadCountResponseFromJson(Map<String, dynamic> json) =>
+    _UnreadCountResponse(count: (json['count'] as num).toInt());
+
+Map<String, dynamic> _$UnreadCountResponseToJson(
+  _UnreadCountResponse instance,
+) => <String, dynamic>{'count': instance.count};
